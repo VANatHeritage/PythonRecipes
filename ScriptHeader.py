ec5eb2db3d2b2694c4d0f7fb67542253f510e4fe
Kirsten Hazler <kirsten.hazler@dcr.virginia.gov>
2017-07-18 11:42:16 -0400

    Sample script header
    Best practice: Always include a header section similar to the one in
    this sample.

---
+++ ScriptHeader.py
@@ -0,0 +1,25 @@
+# ----------------------------------------------------------------------------------------
+# MyScriptExample.py
+# Version:  ArcGIS 10.3.1 / Python 2.7.8
+# Creation Date: 2017-07-18
+# Last Edit: 2017-07-18
+# Creator(s):  Kirsten R. Hazler
+
+# Summary:
+# Write a brief summary of what your script does, here.
+
+# Usage notes:
+# Write any special script or tool usage notes, here.
+
+# Dependencies:
+# List any scripts or custom ArcGIS tools on which this script depends
+
+# Syntax:  
+# MyScriptExample(param1, param2, param3)
+# ----------------------------------------------------------------------------------------
+
+# Best practice: Always include a header section similar to the one above, before the functional portion of the script. You want to make it easy for the next user to know what the script is for, who developed it, and how to use it.
+
+# If the script is being used as part of a custom ArcGIS tool, both ArcGIS and Python versions in use at the time of script development should be noted.
+
+# Make sure to always update the "Last Edit" date when you make revisions.
